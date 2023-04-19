6726b18ccf3e95a3627ee65d86db4481229ac00d
thutasann <thutasann2002@gmail.com>
2023-04-20 01:47:55 +0630

    ui: image lazy load

--- app/page.tsx
+++ app/page.tsx
@@ -8,17 +8,7 @@ export default function Home() {
       <Layout className='pt-0'>
         <div className='flex items-center justify-between w-full'>
           <div className='w-1/2'>
-            <Image
-              src='/thutadev.webp'
-              alt='thutadev'
-              className='w-full h-auto rounded-md'
-              width={1920}
-              height={1920}
-              loading='lazy'
-              placeholder='blur'
-              blurDataURL='/thutadev.webp'
-              quality={100}
-            />
+            <Image src='/thutadev.webp' alt='thutadev' className='w-full h-auto rounded-md' width={1920} height={1920} loading='lazy' />
           </div>
           <div className='w-1/2'>
             <h1>{textData.bannerText.title}</h1>
