2e521c98115dd5484ca0941bbc6ca64a4136d295
thutasann <thutasann2002@gmail.com>
2023-04-20 12:44:49 +0630

    feat: add vercel analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import './globals.css'
 import { Poppins } from 'next/font/google'
 import { NavBar } from '@/components/navbar'
 import Footer from '@/components/footer'
+import { Analytics } from '@vercel/analytics/react'
 
 export const metadata: Metadata = {
   title: userData.about.metaTitle,
@@ -41,6 +42,7 @@ const poppin = Poppins({
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang='en'>
+      <Analytics />
       <body>
         <main className={`${poppin.className} font-poppins bg-light w-full min-h-screen`}>
           <NavBar />
