25b24c9130a86feae8c34207ccda6455daabe132
thutasann <thutasann2002@gmail.com>
2023-04-22 15:11:22 +0630

    ui: experience indicator

--- components/experience/index.tsx
+++ components/experience/index.tsx
@@ -1,12 +1,27 @@
+'use client'
+
 import { experiences } from '@/constants/data'
-import React from 'react'
+import React, { useRef } from 'react'
+import { motion, useScroll } from 'framer-motion'
+import { Icon } from './icon'
 
 const Experience = (): JSX.Element => {
+  const ref = useRef<HTMLDivElement>(null)
+  const { scrollYProgress } = useScroll({
+    target: ref,
+    offset: ['start end', 'center start'],
+  })
+
   return (
     <>
       <h2 className='section-title'>Experience</h2>
-      <div className='w-[75%] mx-auto mt-16 relative'>
-        <div className='absolute left-8 top-0 w-[4px] h-full bg-dark origin-top' />
+      <div ref={ref} className='w-[75%] mx-auto mt-16 relative'>
+        <motion.div
+          style={{
+            scaleY: scrollYProgress,
+          }}
+          className='absolute left-9 top-0 w-[4px] h-[100vh] bg-dark origin-top'
+        />
         <ul className='w-full flex flex-col items-start justify-between ml-4'>
           {experiences.map((exp, idx) => (
             <ExperienceCard
@@ -26,9 +41,23 @@ const Experience = (): JSX.Element => {
 }
 
 const ExperienceCard: React.FC<IExperience> = ({ position, company, companyLink, time, address, work }): JSX.Element => {
+  const ref = useRef<any>(null)
+
   return (
-    <li className='my-8 first:mt-0 last:mb-0 w-[60%] mx-auto flex flex-col items-center justify-between'>
-      <div>
+    <li ref={ref} className='my-8 first:mt-0 last:mb-0 w-[60%] mx-auto flex flex-col items-center justify-between'>
+      <Icon reference={ref} />
+      <motion.div
+        initial={{
+          y: 50,
+        }}
+        whileInView={{
+          y: 0,
+        }}
+        transition={{
+          duration: 0.5,
+          type: 'spring',
+        }}
+      >
         <h3 className='capitalize font-[700] text-2xl'>
           {position}&nbsp;
           <a
@@ -45,7 +74,7 @@ const ExperienceCard: React.FC<IExperience> = ({ position, company, companyLink,
           {time} | {address}
         </span>
         <p className='w-full font-medium'>{work}</p>
-      </div>
+      </motion.div>
     </li>
   )
 }
