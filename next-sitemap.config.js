5913036a03798b84e6761b5ca40a9926ccdfcf9d
thutasann <id006@so4f.org>
2023-04-29 23:01:05 +0630

    uifi: broke

--- next-sitemap.config.js
+++
@@ -1,5 +0,0 @@
-/** @type {import('next-sitemap').IConfig} */
-module.exports = {
-  siteUrl: 'https://thutadev.vercel.app/',
-  generateRobotsTxt: true,
-}
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 
-import { fontFamily } from 'tailwindcss/defaultTheme'
+const { fontFamily } = require('tailwindcss/defaultTheme')
 
 module.exports = {
   content: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}', './app/**/*.{js,ts,jsx,tsx}'],
