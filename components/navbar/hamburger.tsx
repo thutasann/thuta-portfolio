3dae84e9cb41753e089e76a2b12e865ea8b1591e
thutasann <thutasann2002@gmail.com>
2023-05-05 12:43:04 +0630

    fix: accessiblity

--- components/navbar/hamburger.tsx
+++ components/navbar/hamburger.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Hamburger = ({ onClick, isOpen }: IHumburger) => {
   return (
-    <button className='flex-col justify-center items-center hidden lg:flex' onClick={onClick}>
+    <button className='flex-col justify-center items-center hidden lg:flex' onClick={onClick} aria-label='hamburger menu'>
       <span
         className={`bg-dark dark:bg-light transition-all duration-300 ease-out block h-0.5 w-6 rounded-sm ${
           isOpen ? 'rotate-45 translate-y-1' : '-translate-y-0.5'
